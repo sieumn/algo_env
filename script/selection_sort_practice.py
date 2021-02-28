3b1b52a3dc82367da911bb08935bc30423576570
Sieun Park <sieunpark95@gmail.com>
2021-02-28 00:57:47 +0000

    solution: selection sort

--- script/selection_sort_practice.py
+++ script/selection_sort_practice.py
@@ -1,12 +1,22 @@
 # Ploblem : https://www.acmicpc.net/problem/11399
 # [Required] Use the Selection Sort to solve it.
 
-# Solver : 
-# Solved Date : 
-# BigO:
+# Solver : Sieun Park
+# Solved Date : 2021.02.28
+# BigO: O(n2)
 
 def sol(n_list):
+    #selection_sort
+    for i in range(len(n_list)-1):
+        min_index = i
+        for j in range(i+1,len(n_list)):
+            if n_list[j] < n_list[min_index]: min_index = j
+        if i != min_index: n_list[i], n_list[min_index] = n_list[min_index], n_list[i] #swap
+    
+    #calculate total time
     total_time = 0
+    for i in range(len(n_list)):
+        total_time += sum(n_list[:i+1])
     return total_time
 
 if __name__ == '__main__':
--- script/selection_sort_simple.py
+++ script/selection_sort_simple.py
@@ -1,12 +1,17 @@
 # https://www.acmicpc.net/problem/2752
 # [Required] Use the Selection Sort to solve it.
 
-# Solver : 
-# Solved Date : 
-# BigO:
+# Solver : Sieun Park
+# Solved Date : 2021.02.28
+# BigO: O(n2)
 
-
+#selection_sort
 def selection_sort(n_list):
+    for i in range(len(n_list)-1):
+        min_index = i
+        for j in range(i+1, len(n_list)):
+            if n_list[j] < n_list[min_index]: min_index = j
+        if i != min_index: n_list[i], n_list[min_index] = n_list[min_index], n_list[i]
     return n_list
 
 if __name__ == '__main__':
