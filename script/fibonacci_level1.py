c3a06a551390c36c42c831c9bcf4283e3e7508dc
Sieun Park <sieunpark95@gmail.com>
2021-03-01 20:10:56 +0000

    solution: fibonacci_level1.py

--- script/fibonacci_level1.py
+++ script/fibonacci_level1.py
@@ -1,13 +1,13 @@
 # Ploblem : [피보나치 수 5] https://www.acmicpc.net/problem/10870
 
-# Solver : 
-# Solved Date : 
-# BigO:
-
+# Solver : Sieun Park
+# Solved Date : 2021.03.01
+# BigO: O(2^n)
 
 def fibonacci_1(n):
-    return 0
-
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fibonacci_1(n-1)+fibonacci_1(n-2)
 
 if __name__ == '__main__':
     n = int(input())
