8b26d53722254db56d910dac6eaef2ab433aa563
Sieun Park <sieunpark95@gmail.com>
2021-03-02 11:15:55 +0000

    solution: fibonacci_level2.py

--- script/fibonacci_level2.py
+++ script/fibonacci_level2.py
@@ -1,13 +1,18 @@
 # Ploblem : [피보나치 수 4] https://www.acmicpc.net/problem/10826
 
-# Solver : 
-# Solved Date : 
-# BigO:
+# Solver : Sieun Park
+# Solved Date : 2020.03.02
+# BigO: O(n)
 
 
 def fibonacci_2(n):
-    return 0
-
+    if n == 0: return 0
+    elif n == 1: return 1
+    else:
+        t1, t2 = 0, 1
+        for i in range(1,n):
+            t1, t2 = t2, t1 + t2
+        return t2
 
 if __name__ == '__main__':
     n = int(input())
